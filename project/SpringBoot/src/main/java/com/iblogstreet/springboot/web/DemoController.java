40bfa347a33fe8d3318b42988c66ce5008b0298f
wangjun <wang.jun16@163.com>
2018-07-26 13:46:24 +0800

    实现hello spring

---
+++ project/SpringBoot/src/main/java/com/iblogstreet/springboot/web/DemoController.java
@@ -0,0 +1,12 @@
+package com.iblogstreet.springboot.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @RequestMapping("/hello")
+    public String sayhello() {
+        return "hello spring";
+    }
+}
