425f6a84881254a0a1019ad1998637d91cfe0317
wangjun <wang.jun16@163.com>
2018-07-26 16:43:15 +0800

    修改启动banner

--- project/SpringBoot/src/main/java/com/iblogstreet/springboot/web/DemoMvcController.java
+++ project/SpringBoot/src/main/java/com/iblogstreet/springboot/web/DemoMvcController.java
@@ -2,7 +2,6 @@ package com.iblogstreet.springboot.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -16,7 +15,6 @@ import java.util.Map;
 @RestController
 public class DemoMvcController {
     @RequestMapping(value = {"/demomvcindex", "demomvcindex1"}, method = RequestMethod.GET)
-    @ResponseBody
     public Map<String, String> demoMvcIndex() {
         Map<String, String> map = new HashMap<>();
         map.put("river", "is clear");
