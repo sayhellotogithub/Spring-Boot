9bfde011217aa0ff5a5ae874a01f29cc9b68f021
wangjun <wang.jun16@163.com>
2018-07-27 11:26:25 +0800

    将getUserListByAge中查询结果的email 与phone字段赋值互调

--- project/SpringBoot/src/main/java/com/iblogstreet/springboot/dao/UserDao.java
+++ project/SpringBoot/src/main/java/com/iblogstreet/springboot/dao/UserDao.java
@@ -3,6 +3,8 @@ package com.iblogstreet.springboot.dao;
 import com.iblogstreet.springboot.pojo.User;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -22,12 +24,17 @@ public interface UserDao {
      */
     @Select("select id,username,age,phone,email from user where username like concat('%',#{username},'%')")
     List<User> getUserList(String name);
+
     /**
      * 根据用户名来查询用户列表
      *
      * @param age
      * @return
      */
+    @Results(id = "userResult", value = {
+            @Result(property = "email", column = "phone"),
+            @Result(property = "phone", column = "email")
+    })
     @Select("select id,username,age,phone,email from user where age=#{age}")
     List<User> getUserListByAge(int age);
 }
