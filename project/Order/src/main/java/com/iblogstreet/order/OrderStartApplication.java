9ec2ecad05add773c81e9987bf75cf552e77c838
wangjun <wang.jun16@163.com>
2018-07-30 16:04:14 +0800

    实现微服务配制 order为Eureka的客户端 Server为服务端

--- project/Order/src/main/java/com/iblogstreet/order/OrderStartApplication.java
+++ project/Order/src/main/java/com/iblogstreet/order/OrderStartApplication.java
@@ -1,13 +1,14 @@
-package com.iblogstreet.springboot;
+package com.iblogstreet.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class SpringBootStartApplication {
+public class OrderStartApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootStartApplication.class, args);
+        SpringApplication.run(OrderStartApplication.class, args);
     }
 }
